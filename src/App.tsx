3619454a2da9f3bfd01aafb7863ffef3e9a09a85
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-15 13:48:14 -0400

    Test drawing a line graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { select } from 'd3';
+import { select, line, curveCardinal } from 'd3';
 
 function App() {
-  const [data, setData] = useState([25, 1, 45, 60, 23, 40]);
+  const [data, setData] = useState<[number, number][]>([[0,25], [1,1], [4,45], [5,60], [7,23], [8,40]]);
   const svgRef = useRef<SVGSVGElement>(null);
   useEffect(() => { // called when dom elements are rendered
     console.log(svgRef);
     const svg = select(svgRef.current);
-    svg
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("r", value => value)
-      .attr("cx", value => value * 2)
-      .attr("cy", value => value * 2)
-      .attr("stroke", "red");
+    const myLine = line()
+      .x(datum => datum[0] * 50)
+      .y(datum => datum[1])
+      .curve(curveCardinal);
+    svg.selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", value => myLine(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
   }, [data]);
   return <React.Fragment>
     <svg ref={svgRef} width="100%" height="100%"></svg>
     <br/>
-    <button onClick={() => setData(data.map(value => value + 5))}>Update data</button>
-    <button onClick={() => setData(data.filter(value => value < 35))}>Filter Data</button>
   </React.Fragment>;
 }
 
