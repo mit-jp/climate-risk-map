897a483c38d2c9b86134e8bd728e1e770590eb87
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-15 11:36:37 -0400

    Add basic d3 example
    Example from https://www.youtube.com/watch?v=9uEmNgHzPhQ uses the d3 general update pattern with react useRef, useState, and useEffect https://reactjs.org/docs/hooks-effect.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
+import { select } from 'd3';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([25, 1, 45, 60, 23, 40]);
+  const svgRef = useRef<SVGSVGElement>(null);
+  useEffect(() => { // called when dom elements are rendered
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", "red");
+  }, [data]);
+  return <React.Fragment>
+    <svg ref={svgRef} width="100%" height="100%"></svg>
+    <br/>
+    <button onClick={() => setData(data.map(value => value + 5))}>Update data</button>
+    <button onClick={() => setData(data.filter(value => value < 35))}>Filter Data</button>
+  </React.Fragment>;
 }
 
 export default App;
