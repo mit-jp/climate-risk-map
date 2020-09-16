0eb9388c36eb170cad40c0fc676ad38c4755e161
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-16 00:40:47 -0400

    trying to colorize WIP

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,9 +1,12 @@
 import React, { useRef, useEffect } from 'react';
-import { select, geoPath } from 'd3';
+import { select, geoPath, scaleThreshold, schemeBlues } from 'd3';
 import { feature } from 'topojson-client';
 import { Objects, Topology, GeometryCollection } from 'topojson-specification';
 import { GeoJsonProperties } from 'geojson';
 
+const getColor = scaleThreshold<number, string>()
+    .domain([0, 1000000, 2000000, 3000000, 10000000, 100000000, 300000000, 700000000])
+    .range(schemeBlues[9]);
 
 const Map = ({data}: {data: Topology<Objects<GeoJsonProperties>> | undefined}) => {
     const svgRef = useRef<SVGSVGElement>(null);
@@ -26,6 +29,13 @@ const Map = ({data}: {data: Topology<Objects<GeoJsonProperties>> | undefined}) =
             .data(countyGeoJson.features)
             .join("path")
             .attr("class", "county")
+            .attr("fill", d => {
+                if (d.properties) {
+                    return getColor(d.properties["GDP2018"]);
+                } else {
+                    return getColor(0);
+                }
+            })
             .attr("d", geoPath())
     }, [data])
 
