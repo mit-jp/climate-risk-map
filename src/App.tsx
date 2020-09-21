2d5602eae5068ae09b43515bb57fd93fed22ed5b
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-21 15:21:43 -0400

    Change map when data dropdown changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import Map from './Map';
 import { Objects, Topology } from 'topojson-specification';
 import { GeoJsonProperties } from 'geojson';
@@ -11,16 +11,20 @@ type AppState = {
 };
 
 const App = () => {
-  const [{data, selection}, setData] = useState<AppState>({data: undefined, selection: "AWATER"});
+  const [{data, selection}, setState] = useState<AppState>({data: undefined, selection: "AWATER"});
   useEffect(() => {
-    json<Topology<Objects<GeoJsonProperties>>>(process.env.PUBLIC_URL + "/usa-topo.json").then(d => setData({data: d, selection: selection}));
+    json<Topology<Objects<GeoJsonProperties>>>(process.env.PUBLIC_URL + "/usa-topo.json").then(d => setState({data: d, selection: selection}));
   }, []);
+
+  const changeSelection = (event: ChangeEvent<HTMLSelectElement>) => {
+    setState({data: data, selection:  event.target.value});
+  }
 
   return (
     <React.Fragment>
       <h1>Climate Risk Map</h1>
-      <select>
-        <option value="AWATER" selected>Water Area</option>
+      <select value={selection} onChange={changeSelection}>
+        <option value="AWATER">Water Area</option>
         <option value="GDP2018">GDP 2018 (USD)</option>
       </select>
       <Map data={data} selection={selection}/>
