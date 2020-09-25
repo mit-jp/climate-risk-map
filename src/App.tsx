8af6578337ecc45e7c8d050ea79392085fa3bf60
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-25 17:00:58 -0400

    Revert "Revert "Change url when dropdown changes""
    This reverts commit a2c048a31fb88b895102d050c19788fa797afb1f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React from 'react';
 import Home from './Home';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link,
-    useParams
-  } from "react-router-dom";
+    Route  } from "react-router-dom";
 
 const App = () => {
   return (
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path="/:id">
+                <Home />
+            </Route>
+            <Route path="/">
                 <Home />
             </Route>
         </Switch> 
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ import { GeoJsonProperties } from 'geojson';
 import './App.css';
 import { DataName } from './DataTypes';
 import { json } from 'd3-fetch';
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 type AppState = {
   data: Topology<Objects<GeoJsonProperties>> | undefined,
@@ -14,17 +14,29 @@ type AppState = {
 };
 
 const Home = () => {
-  let { id } = useParams<{id: string}>();
-  console.log(id + "  :::  it's working");
+  const history = useHistory();
+  const { id } = useParams<{id: string}>();
+
+  let idEnum: DataName | undefined = DataName[id as keyof typeof DataName]
 
   const [data, setData] = useState<Topology<Objects<GeoJsonProperties>> | undefined>(undefined);
-  const [selection, setSelection] = useState<DataName>(DataName.cmi10_80_1);
+  const [selection, setSelection] = useState<DataName>(idEnum ? idEnum : DataName.cmi10_80_1);
+
   useEffect(() => {
     json<Topology<Objects<GeoJsonProperties>>>(process.env.PUBLIC_URL + "/usa-topo.json").then(setData);
   }, []);
 
+  useEffect(() => {
+    console.log("id changed")
+    let idEnum: DataName | undefined = DataName[id as keyof typeof DataName]
+    if (idEnum) {
+      console.log("setting selection")
+      setSelection(idEnum);
+    }
+  }, [id])
+
   const onSelectionChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setSelection(DataName[event.target.value as keyof typeof DataName]);
+    history.push(event.target.value);
   }
 
   return (
