176b6d69570bb9841658e012811be772ba1f42ee
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-28 13:43:49 -0400

    Add normalized to data definitinos

--- src/DataDefinitions.ts
+++ src/DataDefinitions.ts
@@ -47,6 +47,7 @@ export type DataDefinition = {
     units: string,
     formatter: (n: number | { valueOf(): number }) => string,
     color: ScaleSequential<string> | ScaleThreshold<number, string> | ScaleDiverging<string>,
+    normalized: boolean 
 }
 
 const regularNumber = format(",.0f");
@@ -56,223 +57,260 @@ dataDefinitions.set(DataName.cmi10_00_1, {
     name:"Climate Moisture Index 2000-2019",
     units:"",
     formatter: regularNumber,
-    color: scaleDiverging<string>(scales.interpolateBrBG).domain([-10, 0, 10])
+    color: scaleDiverging<string>(scales.interpolateBrBG).domain([-10, 0, 10]),
+    normalized: true
 });
 dataDefinitions.set(DataName.cmi10_80_9, {
     name:"Climate Moisture Index 1980-1999",
     units:"",
     formatter: regularNumber,
-    color: scaleDiverging<string>(scales.interpolateBrBG).domain([-10, 0, 10])
+    color: scaleDiverging<string>(scales.interpolateBrBG).domain([-10, 0, 10]),
+    normalized: true
 });
 dataDefinitions.set(DataName.cmi10_80_1, {
     name:"Climate Moisture Index 1980-2019",
     units:"",
     formatter: regularNumber,
-    color: scaleDiverging<string>(scales.interpolateBrBG).domain([-10, 0, 10])
+    color: scaleDiverging<string>(scales.interpolateBrBG).domain([-10, 0, 10]),
+    normalized: true
 });
 dataDefinitions.set(DataName.def_00_19, {
     name:"Irrigation Deficit 2000-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleDiverging<string>(x => scales.interpolateBrBG(1-x)).domain([-600, 0, 1600])
+    color: scaleDiverging<string>(x => scales.interpolateBrBG(1-x)).domain([-600, 0, 1600]),
+    normalized: false
 });
 dataDefinitions.set(DataName.def_80_19, {
     name:"Irrigation Deficit 1980-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleDiverging<string>(x => scales.interpolateBrBG(1-x)).domain([-600, 0, 1600])
+    color: scaleDiverging<string>(x => scales.interpolateBrBG(1-x)).domain([-600, 0, 1600]),
+    normalized: false
 });
 dataDefinitions.set(DataName.def_80_99, {
     name:"Irrigation Deficit 1980-1999",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleDiverging<string>(x => scales.interpolateBrBG(1-x)).domain([-600, 0, 1600])
+    color: scaleDiverging<string>(x => scales.interpolateBrBG(1-x)).domain([-600, 0, 1600]),
+    normalized: false
 });
 dataDefinitions.set(DataName.dry_00_19, {
     name:"Drought Indicator 2000-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 1500])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 1500]),
+    normalized: false
 });
 dataDefinitions.set(DataName.dry_80_19, {
     name:"Drought Indicator 1980-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 1500])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 1500]),
+    normalized: false
 });
 dataDefinitions.set(DataName.dry_80_99, {
     name:"Drought Indicator 1980-1999",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 1500])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 1500]),
+    normalized: false
 });
 dataDefinitions.set(DataName.gw_00_19, {
     name:"Groundwater 2000-2019",
     units:"mm/month",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 40])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 40]),
+    normalized: false
 });
 dataDefinitions.set(DataName.gw_80_19, {
     name:"Groundwater 1980-2019",
     units:"mm/month",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 40])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 40]),
+    normalized: false
 });
 dataDefinitions.set(DataName.gw_80_99, {
     name:"Groundwater 1980-1999",
     units:"mm/month",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 40])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 40]),
+    normalized: false
 });
 dataDefinitions.set(DataName.ht_00_19, {
     name:"Maximum Month Temperature 2000-2019",
     units:"°C",
     formatter: regularNumber,
-    color: scaleDiverging<string>(x => scales.interpolateSpectral(1 - x)).domain([20, 30, 40])
+    color: scaleDiverging<string>(x => scales.interpolateSpectral(1 - x)).domain([20, 30, 40]),
+    normalized: false
 });
 dataDefinitions.set(DataName.ht_80_19, {
     name:"Maximum Month Temperature 1980-2019",
     units:"°C",
     formatter: regularNumber,
-    color: scaleDiverging<string>(x => scales.interpolateSpectral(1 - x)).domain([20, 30, 40])
+    color: scaleDiverging<string>(x => scales.interpolateSpectral(1 - x)).domain([20, 30, 40]),
+    normalized: false
 });
 dataDefinitions.set(DataName.ht_80_99, {
     name:"Maximum Month Temperature 1980-1999",
     units:"°C",
     formatter: regularNumber,
-    color: scaleDiverging<string>(x => scales.interpolateSpectral(1 - x)).domain([20, 30, 40])
+    color: scaleDiverging<string>(x => scales.interpolateSpectral(1 - x)).domain([20, 30, 40]),
+    normalized: false
 });
 dataDefinitions.set(DataName.pet_00_19, {
     name:"Mean Annual Potential Evapotranspiration 2000-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([300, 1700])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([300, 1700]),
+    normalized: false
 });
 dataDefinitions.set(DataName.pet_80_19, {
     name:"Mean Annual Potential Evapotranspiration 1980-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([300, 1700])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([300, 1700]),
+    normalized: false
 });
 dataDefinitions.set(DataName.pet_80_99, {
     name:"Mean Annual Potential Evapotranspiration 1980-1999",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([300, 1700])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([300, 1700]),
+    normalized: false
 });
 dataDefinitions.set(DataName.prc_00_19, {
     name: "Mean Annual Precipitation 2000-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2200])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2200]),
+    normalized: false
 });
 dataDefinitions.set(DataName.prc_80_19, {
     name:"Mean Annual Precipitation 1980-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2200])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2200]),
+    normalized: false
 });
 dataDefinitions.set(DataName.prc_80_99, {
     name:"Mean Annual Precipitation 1980-1999",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2200])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2200]),
+    normalized: false
 });
 dataDefinitions.set(DataName.ro_00_19, {
     name:"Mean Annual Runoff 2000-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2000])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.ro_80_19, {
     name:"Mean Annual Runoff 1980-2019",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2000])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.ro_80_99, {
     name:"Mean Annual Runoff 1980-1999",
     units:"mm/year",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2000])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 2000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.wet_00_19, {
     name:"Flood Indicator 2000-2019",
     units:"mm/month",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 500])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 500]),
+    normalized: false
 });
 dataDefinitions.set(DataName.wet_80_19, {
     name:"Flood Indicator 1980-2019",
     units:"mm/month",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 500])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 500]),
+    normalized: false
 });
 dataDefinitions.set(DataName.wet_80_99, {
     name:"Flood Indicator 1890-1999",
     units:"mm/month",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 500])
+    color: scaleSequential<string>(scales.interpolateBlues).domain([0, 500]),
+    normalized: false
 });
 dataDefinitions.set(DataName.GDP2018, {
     name: "GDP 2018",
     units: "USD",
     formatter: format("$,.0f"),
-    color: scaleThreshold<number, string>().domain([0, 1000000, 2000000, 3000000, 10000000, 100000000, 300000000, 700000000]).range(scales.schemeGreens[8])
+    color: scaleThreshold<number, string>().domain([0, 1000000, 2000000, 3000000, 10000000, 100000000, 300000000, 700000000]).range(scales.schemeGreens[8]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Allindustr, {
     name:"Allindustr",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 1000000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 1000000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Farming, {
     name:"Farming",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Mining, {
     name:"Mining",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Constructi, {
     name:"Constructi",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Retailtrad, {
     name:"Retailtrad",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Informatio, {
     name:"Informatio",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.Wholesalet, {
     name:"Wholesalet",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([0, 20000]),
+    normalized: false
 });
 dataDefinitions.set(DataName.discuss, {
     name:"discuss",
     units:"",
     formatter: regularNumber,
-    color: scaleSequential<string>(scales.interpolateGreens).domain([20, 60])
+    color: scaleSequential<string>(scales.interpolateGreens).domain([20, 60]),
+    normalized: false
 });
 dataDefinitions.set(DataName.PerCapitap, {
     name:"PerCapitap",
     units:"",
     formatter: regularNumber,
-    color: scaleDiverging<string>(scales.interpolateBrBG).domain([10000, 40000, 100000])
+    color: scaleDiverging<string>(scales.interpolateBrBG).domain([10000, 40000, 100000]),
+    normalized: false
 });
 
 export default dataDefinitions;
