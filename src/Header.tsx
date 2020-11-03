ed24d51ac89a8ef13b83e70b0dee21df78a65275
Cypress Frankenfeld <iamcypress@gmail.com>
2020-11-02 23:25:18 -0500

    Change title and logo

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import logo from './logo.jpg';
+import mstLogo from './MST-logo.jpg';
+import mitLogo from './MIT-logo.svg';
+import jointProgramLogo from './joint-program-logo.jpg'
 
 const Header = () =>
     <header>
-        <a href="https://globalchange.mit.edu/"><img src={logo} alt="MIT Joint Program on The Science and Policy of Global Change" /></a>
-        <h1>MIT Climate Risk Map</h1>
+        <div id="mst-title">
+            <img src={mstLogo} alt="Multi System Triage" id="mst-logo" />
+            <h1>Multi-Systems Triage</h1>
+        </div>
+        <a href="https://globalchange.mit.edu/"><img src={jointProgramLogo} id="joint-program-logo" alt="MIT Joint Program on The Science and Policy of Global Change" /></a>
+        <a href="https://mit.edu/"><img src={mitLogo} alt="MIT" id="mit-logo" /></a>
     </header>
 
 export default Header;
