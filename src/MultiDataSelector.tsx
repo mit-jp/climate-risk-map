35bd87e4084da42994d546318da9c13a4cadad17
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-15 16:20:06 -0400

    Add accordion expanders in risk metrics list
    Fixes https://github.com/cypressf/mit-climate-data-viz/issues/108

--- src/MultiDataSelector.tsx
+++ src/MultiDataSelector.tsx
@@ -1,7 +1,9 @@
 import React, { ChangeEvent } from 'react';
-import dataDefinitions, { DataIdParams, DataGroup, Normalization, Year, Dataset } from './DataDefinitions';
-import { Map } from 'immutable';
+import dataDefinitions, { DataIdParams, DataGroup, Normalization, Year, Dataset, DataDefinition, DataType } from './DataDefinitions';
+import { Map, OrderedMap } from 'immutable';
 import Slider from '@material-ui/core/Slider';
+import { Accordion, AccordionDetails, AccordionSummary } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 type Props = {
     selection: DataIdParams[],
@@ -38,6 +40,41 @@ const multipleChecked = (dataSelections: DataIdParams[]) => {
     return dataSelections.length > 1;
 }
 
+const checkBox = (dataGroup: DataGroup,
+    shouldBeChecked: (dataGroup: DataGroup) => boolean,
+    onSelectionToggled: (event: ChangeEvent<HTMLInputElement>) => void,
+    definition: DataDefinition,
+    dataSelections: DataIdParams[],
+    onWeightChange: (dataGroup: DataGroup, weight: number) => void,
+    dataWeights: Map<DataGroup, number>) => {
+    return <div key={dataGroup} className={shouldBeChecked(dataGroup) ? "selected-group" : undefined}>
+
+        <input
+            className="data-group"
+            id={dataGroup}
+            checked={shouldBeChecked(dataGroup)}
+            type="checkbox"
+            value={dataGroup}
+            onChange={onSelectionToggled}
+            name="dataGroup" />
+        <label className="data-group" htmlFor={dataGroup}>{definition.name}</label>
+        {shouldBeChecked(dataGroup) && multipleChecked(dataSelections) &&
+            <div className="weight">
+                <div className="weight-label">Weight</div>
+                <Slider
+                    className="weight-slider"
+                    min={0.1}
+                    max={1}
+                    step={0.1}
+                    marks={marks}
+                    valueLabelDisplay="auto"
+                    onChange={(_, weight) => onWeightChange(dataGroup, weight as number)}
+                    value={dataWeights.get(dataGroup) ?? 1} />
+            </div>}
+    </div>;
+}
+
+
 const marks = [{ value: 0.1, label: "min" }, { value: 1, label: "max" }]
 
 const MultiDataSelector = ({ selection: dataSelections, onSelectionChange, onWeightChange, dataWeights }: Props) => {
@@ -57,43 +94,52 @@ const MultiDataSelector = ({ selection: dataSelections, onSelectionChange, onWei
         return selectionMap.has(dataGroup);
     }
 
-    const getDataGroups = () => {
-        return Array.from(dataDefinitions.entries())
-            .filter(([_, definition]) => definition.normalizations.contains(Normalization.Percentile))
+    const getDemographicRisks = () =>
+        Array.from(dataDefinitions.entries())
+            .filter(([_, definition]) =>
+                definition.normalizations.contains(Normalization.Percentile) && 
+                (definition.type === DataType.Economic ||
+                definition.type === DataType.Demographics))
             .map(([dataGroup, definition]) =>
-                <div key={dataGroup} className={shouldBeChecked(dataGroup) ? "selected-group" : undefined}>
-                    <input
-                        className="data-group"
-                        id={dataGroup}
-                        checked={shouldBeChecked(dataGroup)}
-                        type="checkbox"
-                        value={dataGroup}
-                        onChange={onSelectionToggled}
-                        name="dataGroup" />
-                    <label className="data-group" htmlFor={dataGroup}>{definition.name}</label>
-                    {
-                        shouldBeChecked(dataGroup) && multipleChecked(dataSelections) &&
-                        <div className="weight">
-                            <div className="weight-label">Weight</div>
-                            <Slider
-                            className="weight-slider"
-                            min={0.1}
-                            max={1}
-                            step={0.1}
-                            marks={marks}
-                            valueLabelDisplay="auto"
-                            onChange={(_, weight) => onWeightChange(dataGroup, weight as number)}
-                            value={dataWeights.get(dataGroup) ?? 1} />
-                        </div>
-                    }
-
-                </div>
+                checkBox(dataGroup, shouldBeChecked, onSelectionToggled, definition, dataSelections, onWeightChange, dataWeights)
             )
-    }
+
+    const getClimateRisks = () =>
+        Array.from(dataDefinitions.entries())
+            .filter(([_, definition]) =>
+                definition.normalizations.contains(Normalization.Percentile) && 
+                definition.type !== DataType.Economic &&
+                definition.type !== DataType.Demographics)
+            .map(([dataGroup, definition]) =>
+                checkBox(dataGroup, shouldBeChecked, onSelectionToggled, definition, dataSelections, onWeightChange, dataWeights)
+            )
 
     return (
         <form id="data-selector">
-            {getDataGroups()}
+            <Accordion>
+                <AccordionSummary
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                Risk Metrics
+                </AccordionSummary>
+                <AccordionDetails>
+                    {getClimateRisks()}
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    expandIcon={<ExpandMoreIcon />}
+                >
+                Environmental Justice
+                </AccordionSummary>
+                <AccordionDetails>
+                    {getDemographicRisks()}
+                </AccordionDetails>
+            </Accordion>
         </form>
     )
 }
