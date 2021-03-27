a32ff071ca231bbe0b9973b7c1c31ca14abf24ce
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-26 20:37:06 -0400

    Reformat for legibility

--- src/DataDefinitions.ts
+++ src/DataDefinitions.ts
@@ -158,32 +158,73 @@ const getRegularId = (params: DataIdParams) => params.dataGroup as string
 export const datasetDefinitions = Map<Dataset, DatasetDefinition>([
     [Dataset.MERRA2, {
         name: "MERRA-2",
-        description: "The Modern-Era Retrospective analysis for Research and Applications, Version 2 (MERRA-2) provides data beginning in 1980 at a spatial resolution of 0.625° × 0.5°. In comparison with the original MERRA dataset, MERRA-2 represents the advances made in both the Goddard Earth Observing System Model, Version 5 (GEOS- 5) and the Global Statistical Interpolation (GSI) assimilation system that enable assimilation of modern hyperspectral radiance and microwave observations, along with GPS-Radio Occultation datasets. MERRA-2 is the first long-term global reanalysis to assimilate space- based observations of aerosols and represent their interactions with other physical processes in the climate system.",
+        description: `The Modern-Era Retrospective analysis for Research
+            and Applications, Version 2 (MERRA-2) provides data beginning in
+            1980 at a spatial resolution of 0.625° × 0.5°. In comparison
+            with the original MERRA dataset, MERRA-2 represents the advances
+            made in both the Goddard Earth Observing System Model, Version
+            5 (GEOS- 5) and the Global Statistical Interpolation (GSI)
+            assimilation system that enable assimilation of modern hyperspectral
+            radiance and microwave observations, along with GPS-Radio
+            Occultation datasets. MERRA-2 is the first long-term global
+            reanalysis to assimilate space- based observations of aerosols
+            and represent their interactions with other physical processes
+            in the climate system.`,
         link: "https://gmao.gsfc.nasa.gov/reanalysis/MERRA-2/"
     }],
     [Dataset.ERA5, {
         name: "ERA5",
-        description: "ERA5 is the fifth generation of ECMWF atmospheric reanalyses and provides hourly estimates of a large number of global atmospheric, land and oceanic climate variables from 1979 to present. The data cover the Earth on a 30km grid (0.25º of the operational model) and resolve the atmosphere using 137 levels from the surface up to a height of 80km, with additional information about uncertainties for all variables at reduced spatial and temporal resolutions. ERA5 combines vast amounts of historical observations into global estimates using advanced modelling (CY41r2 of ECMWF's Integrated Forecast System) and data assimilation (ten member 4D-Var ensemble) systems. Improvements to ERA5, compared to ERA-Interim, include use of HadISST.2, reprocessed ECMWF climate data records (CDR), and implementation of RTTOV11 radiative transfer. Variational bias corrections have not only been applied to satellite radiances, but also ozone retrievals, aircraft observations, surface pressure, and radiosonde profiles.",
+        description: `ERA5 is the fifth generation of ECMWF atmospheric
+            reanalyses and provides hourly estimates of a large number of
+            global atmospheric, land and oceanic climate variables from 1979
+            to present. The data cover the Earth on a 30km grid (0.25º of
+            the operational model) and resolve the atmosphere using 137
+            levels from the surface up to a height of 80km, with
+            additional information about uncertainties for all variables
+            at reduced spatial and temporal resolutions. ERA5 combines
+            vast amounts of historical observations into global estimates
+            using advanced modelling (CY41r2 of ECMWF's Integrated Forecast
+            System) and data assimilation (ten member 4D-Var ensemble)
+            systems. Improvements to ERA5, compared to ERA-Interim, include
+            use of HadISST.2, reprocessed ECMWF climate data records (CDR),
+            and implementation of RTTOV11 radiative transfer. Variational
+            bias corrections have not only been applied to satellite radiances,
+            but also ozone retrievals, aircraft observations, surface pressure,
+            and radiosonde profiles.`,
         link: "https://www.ecmwf.int/en/forecasts/datasets/reanalysis-datasets/era5"
     }],
     [Dataset.NARR, {
         name: "NARR",
-        description: "The NCEP North American Regional Reanalysis (NARR) is a high-resolution (32 km) data set focused upon the North American domain and spanning from 1979 to near present. The NARR uses the high resolution NCEP Eta Model (32km/45 layer) together with the Regional Data Assimilation System (RDAS) which directly assimilates observed precipitation along with other variables. Relative to the NCEP-DOE Global Reanalysis 2, it has a much improved land-hydrology, diurnal cycle and land-atmosphere interaction.",
+        description: `The NCEP North American Regional Reanalysis (NARR) is
+            a high-resolution (32 km) data set focused upon the North American
+            domain and spanning from 1979 to near present. The NARR uses the high
+            resolution NCEP Eta Model (32km/45 layer) together with the Regional
+            Data Assimilation System (RDAS) which directly assimilates observed
+            precipitation along with other variables. Relative to the NCEP-DOE
+            Global Reanalysis 2, it has a much improved land-hydrology, diurnal
+            cycle and land-atmosphere interaction.`,
         link: "https://psl.noaa.gov/data/gridded/data.narr.html"
     }],
     [Dataset.Yale, {
         name: "Yale Program on Climate Change Communication",
-        description: "Statistical estimates of U.S. climate change beliefs, risk perceptions, and policy preferences at the state and local levels.",
+        description: `Statistical estimates of U.S. climate change beliefs,
+            risk perceptions, and policy preferences at the state and local levels.`,
         link: "https://climatecommunication.yale.edu/visualizations-data/ycom-us/"
     }],
     [Dataset.BEA, {
         name: "US Bureau of Economic Analysis",
-        description: "The Bureau of Economic Analysis provides official macroeconomic and industry statistics, most notably reports about the gross domestic product of the United States and its various units—states, cities/towns/townships/villages/counties and metropolitan areas.",
+        description: `The Bureau of Economic Analysis provides official macroeconomic
+            and industry statistics, most notably reports about the gross domestic
+            product of the United States and its various units—states,
+            cities/towns/townships/villages/counties and metropolitan areas.`,
         link: "https://www.bea.gov/data/"
     }],
     [Dataset.Census, {
         name: "US Census Bureau",
-        description: "The Census Bureau is responsible for producing data about the American people and economy. It continually conducts over 130 surveys and programs a year, including the American Community Survey, the U.S. Economic Census, and the Current Population Survey.",
+        description: `The Census Bureau is responsible for producing data about
+            the American people and economy. It continually conducts over 130 surveys
+            and programs a year, including the American Community Survey, the U.S.
+            Economic Census, and the Current Population Survey.`,
         link: "https://www.census.gov/data.html"
     }],
 ]);
