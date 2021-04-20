786baca71bf87c7fb91c9a06b6e5ec245c9ca7ba
Cypress Frankenfeld <iamcypress@gmail.com>
2021-04-20 11:17:31 -0400

    Don't include serializable middleware
    It makes the site super slow in dev

--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,10 @@ export const store = configureStore({
   reducer: {
     app: appReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
