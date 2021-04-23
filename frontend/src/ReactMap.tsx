4a63f70c1aac72da458a41349a3b49e298b0e519
Cypress Frankenfeld <iamcypress@gmail.com>
2021-04-23 18:09:32 -0400

    Draw colored map

--- frontend/src/ReactMap.tsx
+++ frontend/src/ReactMap.tsx
@@ -5,6 +5,9 @@ import { feature, mesh } from 'topojson-client';
 import { GeometryCollection } from 'topojson-specification';
 import { GeoJsonProperties } from 'geojson';
 import { geoPath } from 'd3';
+import DataProcessor from './DataProcessor';
+import Color from './Color';
+
 
 const ReactMap = () => {
     const {
@@ -25,7 +28,14 @@ const ReactMap = () => {
         selections: state.app.dataSelections[state.app.dataTab] ?? [],
     }));
 
-    if (map === undefined) {
+    if (selections.length === 0) {
+        return null;
+    }
+
+    const processedData = DataProcessor(data, selections, dataWeights, state);
+    const colorScheme = Color(selections, detailedView);
+
+    if (map === undefined || processedData === undefined) {
         return null;
     }
 
@@ -34,25 +44,31 @@ const ReactMap = () => {
         map,
         map.objects.counties as GeometryCollection<GeoJsonProperties>
     ).features;
+    const color = (countyId: string) => {
+        const value = processedData.get(countyId);
+        return colorScheme(value as any) ?? MISSING_DATA_COLOR;
+    }
     const countyPaths = counties.map(county => (
         <path
-        key={county.id}
-        // fill={color(county.properties.density)}
-        d={path(county)!}
-      />
+            key={county.id}
+            fill={color(county.id as string)}
+            d={path(county)!}
+        />
     ));
 
     return (
         <div id="map">
             {map &&
-            <svg viewBox="0, 0, 1175, 610">
-                <g className="counties">
-                    {countyPaths}
-                </g>
-            </svg>
+                <svg viewBox="0, 0, 1175, 610">
+                    <g className="counties">
+                        {countyPaths}
+                    </g>
+                </svg>
             }
         </div>
     );
 };
+
+const MISSING_DATA_COLOR = "#ccc";
 
 export default ReactMap;
