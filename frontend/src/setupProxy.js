4f61142e158f69197263846a7b828bf86011dcd0
Cypress Frankenfeld <iamcypress@gmail.com>
2021-07-15 18:14:23 -0400

    Proxy `3000:/api` to `:8080/`
    This is so we can `cargo run` the backend and use its endpoints from `/api` on the frontend in development. When deployed, an apache server will sit in front of the backend and forward `/api` requests to it.
    
    Part of  https://github.com/cypressf/mit-climate-data-viz/issues/191

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+            pathRewrite: { '^/api': '' },
+        })
+    );
+};
