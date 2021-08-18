85f04c93ab796475450a3bde6cfa3822e70020a8
Cypress Frankenfeld <iamcypress@gmail.com>
2021-08-18 10:47:55 -0400

    Add PDF

--- frontend/src/ChoroplethMap.tsx
+++ frontend/src/ChoroplethMap.tsx
@@ -1,7 +1,7 @@
 import { TopoJson } from "./Home";
 import React from "react";
 import { Map } from "immutable";
-import { DataDefinition, DataGroup, DataIdParams, Normalization } from "./DataDefinitions";
+import { DataDefinition, DataGroup, DataIdParams, DataType, Normalization } from "./DataDefinitions";
 import { geoPath } from 'd3';
 import { feature } from 'topojson-client';
 import { GeometryCollection } from 'topojson-specification';
@@ -10,8 +10,41 @@ import Color from "./Color";
 import StateMap from "./StateMap";
 import Legend from "./Legend";
 import { getDataDefinitions } from "./FullMap";
+import ProbabilityDensity from "./ProbabilityDensity";
 
 const MISSING_DATA_COLOR = "#ccc";
+
+
+const getLegendTicks = (selectedDataDefinitions: DataDefinition[], selections: DataIdParams[]) => {
+    const normalization = selections[0].normalization;
+    switch (normalization) {
+        case Normalization.Raw: return selectedDataDefinitions[0].legendTicks;
+        case Normalization.Percentile: return undefined;
+    }
+}
+
+const shouldShowPdf = (selection: DataIdParams, numSelections: number) => {
+    if (selection.dataGroup === DataGroup.Populationpersquaremile2010) {
+        return false;
+    }
+    if (selection.normalization === Normalization.Percentile) {
+        return numSelections > 1;
+    }
+    return true;
+}
+
+const getPdfDomain = (selections: DataIdParams[]) => {
+    const firstSelection = getDataDefinitions(selections)[0];
+    if (firstSelection === undefined) {
+        return undefined;
+    }
+
+    if (firstSelection.type === DataType.ClimateOpinions) {
+        return [0, 100] as [number, number];
+    }
+
+    return undefined;
+}
 
 type Props = {
     map: TopoJson,
@@ -36,6 +69,10 @@ const ChoroplethMap = ({ map, selections, data, detailedView, title, legendForma
         map.objects.counties as GeometryCollection<GeoJsonProperties>
     ).features;
     const ticks = getLegendTicks(getDataDefinitions(selections), selections);
+    const getArrayOfData = () =>
+        Array
+            .from(data.valueSeq())
+            .filter(value => value !== undefined) as number[];
 
     return (
         <React.Fragment>
@@ -54,26 +91,18 @@ const ChoroplethMap = ({ map, selections, data, detailedView, title, legendForma
                 color={colorScheme}
                 tickFormat={legendFormatter}
                 ticks={ticks} />
+            {
+                shouldShowPdf(selections[0], selections.length) &&
+                <ProbabilityDensity
+                    data={getArrayOfData()}
+                    selections={selections}
+                    xRange={getPdfDomain(selections)}
+                    formatter={legendFormatter}
+                    continuous={detailedView}
+                />
+            }
         </React.Fragment>
     )
-}
-
-const getLegendTicks = (selectedDataDefinitions: DataDefinition[], selections: DataIdParams[]) => {
-    const normalization = selections[0].normalization;
-    switch (normalization) {
-        case Normalization.Raw: return selectedDataDefinitions[0].legendTicks;
-        case Normalization.Percentile: return undefined;
-    }
-}
-
-function shouldShowPdf(selection: DataIdParams, numSelections: number) {
-    if (selection.dataGroup === DataGroup.Populationpersquaremile2010) {
-        return false;
-    }
-    if (selection.normalization === Normalization.Percentile) {
-        return numSelections > 1;
-    }
-    return true;
 }
 
 export default ChoroplethMap;
