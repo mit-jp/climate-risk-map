36567aede3b7812fc92da651db40088aa51872bb
Cypress Frankenfeld <iamcypress@gmail.com>
2021-09-30 14:06:15 -0400

    Add more options to map editor

--- frontend/src/editor/MapOptions.tsx
+++ frontend/src/editor/MapOptions.tsx
@@ -1,6 +1,6 @@
-import { Autocomplete, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
+import { Autocomplete, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Checkbox } from "@mui/material";
 import { useGetColorPalettesQuery, useGetScaleTypesQuery, useUpdateMapVisualizationMutation } from "../MapApi";
-import { MapType, MapVisualization } from "../MapVisualization";
+import { FormatterType, MapType, MapVisualization } from "../MapVisualization";
 
 const MapOptions = ({ mapVisualization }: { mapVisualization: MapVisualization }) => {
     const { data: colorPalettes } = useGetColorPalettesQuery(undefined);
@@ -42,6 +42,31 @@ const MapOptions = ({ mapVisualization }: { mapVisualization: MapVisualization }
                 sx={{ width: 300 }}
                 renderInput={(params) => <TextField {...params} label="Color Palette" />}
             />}
+            <FormControlLabel
+                control={<Checkbox
+                    checked={mapVisualization.reverse_scale}
+                    onChange={(_, reverse_scale) => updateMap({ id, reverse_scale })}
+                />}
+                label="Invert" />
+        </FormControl>
+        <FormControlLabel
+            control={<Checkbox
+                checked={mapVisualization.show_pdf}
+                onChange={(_, show_pdf) => updateMap({ id, show_pdf })}
+            />}
+            label="Show Probability Density" />
+        <FormControl component="fieldset">
+            <FormLabel component="legend">Formatter</FormLabel>
+            <RadioGroup
+                aria-label="formatter"
+                value={mapVisualization.formatter_type}
+                name="formatter"
+                onChange={(_, formatter_type) => updateMap({ id, formatter_type: parseInt(formatter_type) })}
+            >
+                <FormControlLabel value={FormatterType.DEFAULT} control={<Radio />} label="Default" />
+                <FormControlLabel value={FormatterType.MONEY} control={<Radio />} label="Money" />
+                <FormControlLabel value={FormatterType.NEAREST_SI_UNIT} control={<Radio />} label="Nearest SI Unit" />
+            </RadioGroup>
         </FormControl>
     </form>;
 }
