e8c88eee5ee4accf784a5ae04ef33e5b6261dade
Cypress Frankenfeld <iamcypress@gmail.com>
2021-10-27 23:31:56 -0400

    Add Tooltip

--- test-svelte/src/MapSelector.svelte
+++ test-svelte/src/MapSelector.svelte
@@ -1,11 +1,13 @@
 <script type="ts">
     import { getMapConfigs } from "./MapConfigApi";
-    import type { MapVisualization } from "./MapConfigApi";
+    import type { MapConfig } from "./MapConfigApi";
+    import ColorBar from "./ColorBar.svelte";
+    import { simpleColor } from "./Color";
 
     const mapConfigs = getMapConfigs();
     let mapConfigId = 0;
-    export let mapConfig: MapVisualization | undefined;
 
+    export let mapConfig: MapConfig | undefined = undefined;
     $: (async () => (mapConfig = (await mapConfigs)[mapConfigId]))();
 </script>
 
@@ -15,15 +17,19 @@
     {:then mapConfigs}
         <form>
             {#each Object.values(mapConfigs) as mapConfig (mapConfig.id)}
-                <label>
+                <div class="map-config">
                     <input
                         type="radio"
                         name="map-config"
                         bind:group={mapConfigId}
                         value={mapConfig.id}
+                        id={mapConfig.id.toString()}
                     />
-                    {mapConfig.name}
-                </label>
+                    <label for={mapConfig.id.toString()}>
+                        <div class="config-name">{mapConfig.name}</div>
+                        <ColorBar colorScheme={simpleColor(mapConfig)} />
+                    </label>
+                </div>
             {/each}
         </form>
     {:catch error}
@@ -33,9 +39,30 @@
 
 <style>
     #map-configs {
-        width: 900px;
+        flex-shrink: 0;
+        height: 100vh;
+        margin: 0;
+        overflow-y: auto;
+        width: 500px;
+    }
+    form {
+        margin: 0;
+    }
+    input {
+        display: none;
+    }
+    input:checked + label,
+    label:hover {
+        background: rgb(211, 211, 211);
+    }
+    .map-config {
+        display: flex;
+        flex-direction: column;
     }
     label {
-        display: block;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        padding: 0.5em;
     }
 </style>
