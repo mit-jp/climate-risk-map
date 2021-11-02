62c82edf5f02ead348cf0f53945be2a3af5bbba4
Cypress Frankenfeld <iamcypress@gmail.com>
2021-11-01 22:13:41 -0400

    Add svelte to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), svelte()],
     build: { outDir: "./build" },
     server: {
         proxy: {
