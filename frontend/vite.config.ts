97b10a23fca126a012b5f951cd32ccc1cbc66275
Cypress Frankenfeld <iamcypress@gmail.com>
2021-12-02 18:52:32 -0500

    Analyze size of bundle

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     build: { outDir: "./build" },
     server: {
         proxy: {
