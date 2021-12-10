fcd09d9f1149a16ab70f869a224887f2c5024e74
Cypress Frankenfeld <iamcypress@gmail.com>
2021-12-10 16:43:56 -0500

    Really basic stub of a test for home
    Tests to see that the home displays the map title for the default map loaded

--- frontend/setupTests.ts
+++ frontend/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import 'whatwg-fetch';
+// to support `Legend.tsx` because it uses canvas, learn more: https://stackoverflow.com/a/55141040/382892
+import 'jest-canvas-mock';
