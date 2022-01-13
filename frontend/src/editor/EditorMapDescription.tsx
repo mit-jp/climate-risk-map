b252d5e9ad2e6e9a81126d168c1fe4a2f37beded
Cypress Frankenfeld <iamcypress@gmail.com>
2022-01-13 08:58:26 -0800

    Remove unused code
    EditorMapDescription is a work in progress. There was some no-op code in it.

--- frontend/src/editor/EditorMapDescription.tsx
+++ frontend/src/editor/EditorMapDescription.tsx
@@ -3,11 +3,8 @@ import { TextField } from '@mui/material'
 import { useState } from 'react'
 import { MapVisualization } from '../MapVisualization'
 
-type DatasetDiff = { id: number; description: string }
-const updateDataset = (diff: DatasetDiff) => null
-
 function EditorMapDescription({ selectedMap }: { selectedMap: MapVisualization }) {
-    const [description, setDescription] = useState(selectedMap.description)
+    const [, setDescription] = useState(selectedMap.description)
     const isLoading = false
 
     return (
@@ -19,11 +16,7 @@ function EditorMapDescription({ selectedMap }: { selectedMap: MapVisualization }
                 fullWidth
                 onChange={(e) => setDescription(e.target.value)}
             />
-            <LoadingButton
-                onClick={() => updateDataset({ id: selectedMap.dataset, description })}
-                loading={isLoading}
-                variant="contained"
-            >
+            <LoadingButton loading={isLoading} variant="contained">
                 save
             </LoadingButton>
         </form>
