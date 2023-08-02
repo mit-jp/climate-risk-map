3e9b15dad961199d60dddd002c3d05d77e9d1fc5
Cypress Frankenfeld <iamcypress@gmail.com>
2023-08-01 23:16:57 -0400

    Draw county colors

--- frontend/src/CanvasMap.tsx
+++ frontend/src/CanvasMap.tsx
@@ -1,17 +1,22 @@
 import { geoPath, select } from 'd3'
+import type { FeatureCollection, GeoJsonProperties, Geometry } from 'geojson'
 import { Map } from 'immutable'
 import { useEffect, useRef } from 'react'
 import * as topojson from 'topojson-client'
 import { MapVisualization } from './MapVisualization'
 
-import { GeoId } from './appSlice'
+import Color from './Color'
+import { getDomain } from './DataProcessor'
 import { TopoJson } from './TopoJson'
+import { GeoId } from './appSlice'
 
 type Props = {
     us: TopoJson
     selection: MapVisualization | undefined
     data: Map<GeoId, number>
 }
+
+const MISSING_DATA_COLOR = '#ccc'
 
 /** Use a canvas d3 renderer instead of drawing everything in an svg */
 export default function CanvasMap({ us, selection, data }: Props) {
@@ -27,18 +32,39 @@ export default function CanvasMap({ us, selection, data }: Props) {
         context.lineJoin = 'round'
         context.lineCap = 'round'
 
-        // Counties
-        context.beginPath()
-        path(
-            topojson.mesh(
-                us,
-                us.objects.counties,
-                (a, b) => a !== b && ((a.id / 1000) | 0) === ((b.id / 1000) | 0)
+        const drawFullMap = (us: TopoJson, mapSpec: MapVisualization, data: Map<GeoId, number>) => {
+            const counties = (
+                topojson.feature(us, us.objects.counties) as FeatureCollection<
+                    Geometry,
+                    GeoJsonProperties
+                >
+            ).features
+            const colorScale = Color(false, true, mapSpec, getDomain(data))
+            counties.forEach((county) => {
+                const value = data.get(Number(county.id))
+                context.beginPath()
+                path(county)
+                context.fillStyle = value != null ? colorScale(value) : MISSING_DATA_COLOR
+                context.fill()
+            })
+        }
+
+        const drawEmptyMap = (us: TopoJson) => {
+            context.beginPath()
+            path(
+                topojson.mesh(
+                    us,
+                    us.objects.counties,
+                    (a, b) => a !== b && ((a.id / 1000) | 0) === ((b.id / 1000) | 0)
+                )
             )
-        )
-        context.lineWidth = 0.5
-        context.strokeStyle = '#aaa'
-        context.stroke()
+            context.lineWidth = 0.5
+            context.strokeStyle = '#aaa'
+            context.stroke()
+        }
+
+        // Counties
+        selection == null ? drawEmptyMap(us) : drawFullMap(us, selection, data)
 
         // States
         context.beginPath()
