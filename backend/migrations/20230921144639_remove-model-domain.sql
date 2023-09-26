6e1d67ec68e82a354024fa275626f3aadcca1f91
ethanrdsch <ethanrdsch01@gmail.com>
2023-09-26 10:43:18 -0400

    remove accidental addition of future data table

--- backend/migrations/20230921144639_remove-model-domain.sql
+++
@@ -1,3 +0,0 @@
--- Remove domain from future model table, as future data already has a start and end date
-ALTER TABLE
-    future_model DROP COLUMN domain;
--- backend/migrations/20230921145028_description-for-model.sql
+++
@@ -1,5 +0,0 @@
--- Add column for a description
-ALTER TABLE
-    future_model
-ADD
-    COLUMN description TEXT;
