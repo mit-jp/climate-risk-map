bd61983d1e557ad0aeb8b3510f67396d3d93a58d
ethanrdsch <ethanrdsch01@gmail.com>
2025-11-06 11:32:02 -0500

    add new subcategories for 2025 outlook data, allow editing subcategory in gui

---
+++ backend/migrations/20251106112254_add-outlook-subcat.sql
@@ -0,0 +1,13 @@
+-- add new subcateogories for 2025 outlook data
+INSERT INTO subcateogory (id, name, order)
+VALUES
+  (4, "Economic Indicators", 4),
+  (5, "GHG Emissions", 5),
+  (6, "Primary Energy Use", 6),
+  (7, "Electricity Production", 7),
+  (8, "Land Use", 8),
+  (9, "Air Pollutant Emissions", 9),
+  (10, "Agricultural and Food Outputs", 10),
+  (11, "Agricultural and Food Prices", 11),
+  (12, "Energy Prices", 12),
+  (13, "Climate", 13);
