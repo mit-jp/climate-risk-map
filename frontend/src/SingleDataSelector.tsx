36731193dcc55e02375f3e45f2363c17a7b3e2ee
ethanrdsch <ethanrdsch01@gmail.com>
2025-12-08 12:02:50 -0500

    allow subcategories to display without normalization

--- frontend/src/SingleDataSelector.tsx
+++ frontend/src/SingleDataSelector.tsx
@@ -1,3 +1,5 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
 import { ChangeEvent } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import css from './DataSelector.module.css'
@@ -6,6 +8,7 @@ import { MapVisualization, MapVisualizationId } from './MapVisualization'
 import YearSelector, { readable } from './YearSelector'
 import { changeDataSource, changeDateRange, changeMapSelection, selectSelections } from './appSlice'
 import { RootState } from './store'
+import { useGetSubcategoriesQuery } from './MapApi'
 
 function SingleDataSelector({ maps }: { maps: Record<MapVisualizationId, MapVisualization> }) {
     const selection = useSelector((state: RootState) =>
@@ -13,6 +16,7 @@ function SingleDataSelector({ maps }: { maps: Record<MapVisualizationId, MapVisu
     )
 
     const dispatch = useDispatch()
+    const { data: subcategories } = useGetSubcategoriesQuery(undefined)
 
     const onDataSourceChange = (event: ChangeEvent<HTMLInputElement>) => {
         const dataset = parseInt(event.target.value, 10)
@@ -38,45 +42,71 @@ function SingleDataSelector({ maps }: { maps: Record<MapVisualizationId, MapVisu
         selection.mapVisualization === map.id &&
         Object.keys(map.sources).length > 1
 
+    const isEmpty = (subcategoryId: number) =>
+        Object.values(maps).filter((map) => map.subcategory === subcategoryId).length === 0
+
+    const renderMapEntry = (map: MapVisualization) => (
+        <div key={map.id}>
+            <input
+                className={css.input}
+                id={map.id.toString()}
+                checked={selection?.mapVisualization === map.id}
+                type="radio"
+                value={map.id}
+                onChange={onMapSelectionChange}
+                name="dataGroup"
+            />
+            <label className={css.label} htmlFor={map.id.toString()}>
+                <div className={css.name}>{map.displayName}</div>
+                {selection !== undefined && shouldShowYearLabel(map) && (
+                    <div className={css.year}>{readable(selection.dateRange)}</div>
+                )}
+            </label>
+            {selection !== undefined && shouldShowYearSelector(map) && (
+                <YearSelector
+                    id={map.id.toString()}
+                    years={map.date_ranges_by_source[selection.dataSource]}
+                    selectedYear={selection.dateRange}
+                    onChange={(dateRange) => dispatch(changeDateRange(dateRange))}
+                />
+            )}
+            {selection !== undefined && shouldShowDatasets(map) && (
+                <DataSourceSelector
+                    id={map.id.toString()}
+                    dataSources={Object.values(map.sources)}
+                    selectedDataSource={selection.dataSource}
+                    onSelectionChange={onDataSourceChange}
+                />
+            )}
+        </div>
+    )
+
+    const getDataList = (filterFn: (map: MapVisualization) => boolean) =>
+        Object.values(maps)
+            .sort((a, b) => a.order - b.order)
+            .filter((map) => filterFn(map))
+            .map((map) => renderMapEntry(map))
+
     return (
         <form id={css.dataSelector}>
-            {Object.values(maps)
-                .sort((a, b) => a.order - b.order)
-                .map((map) => (
-                    <div key={map.id}>
-                        <input
-                            className={css.input}
-                            id={map.id.toString()}
-                            checked={selection?.mapVisualization === map.id}
-                            type="radio"
-                            value={map.id}
-                            onChange={onMapSelectionChange}
-                            name="dataGroup"
-                        />
-                        <label className={css.label} htmlFor={map.id.toString()}>
-                            <div className={css.name}>{map.displayName}</div>
-                            {selection !== undefined && shouldShowYearLabel(map) && (
-                                <div className={css.year}>{readable(selection.dateRange)}</div>
-                            )}
-                        </label>
-                        {selection !== undefined && shouldShowYearSelector(map) && (
-                            <YearSelector
-                                id={map.id.toString()}
-                                years={map.date_ranges_by_source[selection.dataSource]}
-                                selectedYear={selection.dateRange}
-                                onChange={(dateRange) => dispatch(changeDateRange(dateRange))}
-                            />
-                        )}
-                        {selection !== undefined && shouldShowDatasets(map) && (
-                            <DataSourceSelector
-                                id={map.id.toString()}
-                                dataSources={Object.values(map.sources)}
-                                selectedDataSource={selection.dataSource}
-                                onSelectionChange={onDataSourceChange}
-                            />
-                        )}
-                    </div>
-                ))}
+            {subcategories &&
+                subcategories
+                    .filter((subcategory) => !isEmpty(subcategory.id))
+                    .map((subcategory) => (
+                        <Accordion key={subcategory.id} defaultExpanded>
+                            <AccordionSummary
+                                aria-controls={`subcategory-${subcategory.id}-content`}
+                                id={`subcategory-${subcategory.id}-header`}
+                                expandIcon={<ExpandMoreIcon />}
+                            >
+                                {subcategory.name}
+                            </AccordionSummary>
+                            <AccordionDetails style={{ padding: 0 }}>
+                                {getDataList((map) => map.subcategory === subcategory.id)}
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
+            {getDataList((map) => map.subcategory == null)}
         </form>
     )
 }
