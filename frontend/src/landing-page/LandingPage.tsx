e7d2c51e3bd3e1a39a09f91ac6656b9aae0ee794
Thornblaster12345 <alexvinuk@icloud.com>
2026-08-21 16:19:05 -0400

    finished revamping about page

--- frontend/src/landing-page/LandingPage.tsx
+++ frontend/src/landing-page/LandingPage.tsx
@@ -1,6 +1,10 @@
 import { Button, Container, Typography } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
 import css from './LandingPage.module.css'
+import landingpage1 from './images/about_page_img1.png'
+import landingpage2 from './images/about_page_img2.png'
+import landingpage3 from './images/about_page_img3.png'
+import landingpage4 from './images/about_page_img4.png'
 import Header from '../Header'
 import LandingPageNavbar from './LandingPageNavbar'
 
@@ -15,10 +19,10 @@ function LandingPage() {
                         Welcome to the STRESS Platform!
                     </Typography>
                     <Typography variant="h6" component="p" className={css.subtitle}>
-                        STRESS is a web-based platform that allows users to explore and visualize
-                        the relative risk of climate change impacts across the United States. To
-                        learn more, feel free to explore using the tabs above! Otherwise, click the
-                        button below to launch STRESS!
+                        The System for the Triage of Risks from Environmental and Socioeconomic
+                        Stressors (STRESS) is an interactive mapping platform that allows users to
+                        identify where there are relatively high levels of various socioeconomic and
+                        environmental risk factors.
                     </Typography>
                     <Button component={RouterLink} to="/" variant="contained" size="large">
                         Launch the STRESS Platform
@@ -26,56 +30,72 @@ function LandingPage() {
                 </Container>
                 <Container className={css.container}>
                     <Typography variant="h4" component="h1" className={css.title}>
-                        What is STRESS?
+                        Why was STRESS created?
                     </Typography>
                     <Typography variant="h6" component="p" className={css.subtitle}>
                         Environmental, climate, and socioeconomic factors overlap spatially and
                         temporally to create areas of heightened susceptibility to harm. For
                         example, negative health impacts from heat and air pollution can compound
-                        and these impacts will be greatest in places with an older population and
-                        more outdoor workers. Capturing the evolving relationships between different
-                        factors is challenging and resource intensive, necessitating data
-                        collection, complex modeling, and deep qualitative work. We want to keep
-                        communities safe, but we do not have the resources to invest in resiliency
-                        measures everywhere, nor do we have the resources to rigorously understand
-                        all of these interactions everywhere. Given this, it is desirable to create
-                        relative risk indicators that summarize and aggregate data from various
-                        sources to show where there are relatively high levels of various metrics.
-                        Resources – ranging from material investment in resiliency to further
-                        research – can then be allocated to ‘hotspots’ with high indication of
-                        relative risk, where they might have the greatest impact in reducing risk.
-                        Thus the Center for Sustainability Science and Strategy (CS3) created
-                        STRESS, the System for the Triage of Risks from Environmental and
-                        Socioeconomic Stressors.
+                        and have larger impacts in lower-income areas. It is important to identify
+                        these areas to prioritize risk mitigation efforts, but capturing the
+                        evolving relationships between different factors is challenging and resource
+                        intensive, necessitating data collection, complex modeling, and deep
+                        qualitative work.
+                    </Typography>
+                    <img
+                        src={landingpage1}
+                        alt="Different maps aggregated into one"
+                        className={css.aboutUsImg}
+                    />
+                    <Typography variant="h6" component="p" className={css.subtitle}>
+                        There are finite resources for research and intervention, so it is desirable
+                        to have an efficient method to aggregate data for various metrics from
+                        various sources and identify ‘relative risk hotspots’. STRESS was created to
+                        meet this need by providing a platform for users to create relative risk
+                        indicators that can then be used to inform further research or resource
+                        allocation.
                     </Typography>
                     <Typography variant="h4" component="h1" className={css.title}>
-                        How does STRESS work?
+                        What are STRESS’s current capabilities?
                     </Typography>
                     <Typography variant="h6" component="p" className={css.subtitle}>
-                        STRESS is a publicly available platform that enables people to select,
-                        weight, and combine various socioeconomic and environmental factors to
-                        identify these ‘risk hotspots.’ STRESS currently allows users to create
-                        ‘combined relative risk scores’ for counties across the United States. Users
-                        can choose amongst over 20 datasets representing aspects of socioeconomic
-                        and environmental risk. The platform percentile-ranks each county for each
-                        metric, then averages these percentiles using the users’ weighting to
-                        calculate the combinatory risk score. Users are immediately shown a USA-wide
-                        county level map of these relative risks. For more information, users can
-                        click to zoom into a county; they will see relative risk scores now
-                        calculated only relative to other counties in the same state and they can
-                        view a ‘report card’ for their county. While many risk and vulnerability
-                        index mapping platforms exist, STRESS differentiates itself by allowing
-                        users to select and weight metrics for themselves, as opposed to providing
-                        pre-constructed indices. This makes it adaptable to the needs of a diverse
-                        user base with differing values, interests, background knowledge, goals, and
-                        abilities to intervene. In addition to allowing users to combine metrics,
-                        STRESS provides a centralized platform where users can easily view over 100
-                        USA wide county-level datasets, over 20 country-level global datasets, and
-                        several city-level datasets from local partnerships. Although STRESS does
-                        not produce its own data, it provides an important service by bringing
-                        datasets from various agencies into one platform to increase accessibility.
-                        We are continually updating STRESS with a broad range of up-to-date datasets
-                        and forming new partnerships.
+                        STRESS currently allows users to create ‘combined relative risk’ scores for
+                        counties across the United States by choosing amongst over 20 datasets
+                        representing aspects of socioeconomic and environmental risk. The platform
+                        percentile-ranks each county for each metric, then uses a user-weighted
+                        average to calculate the combinatory risk score. Users are immediately shown
+                        a map of these relative risks.
+                    </Typography>
+                    <img
+                        src={landingpage2}
+                        alt="Different maps aggregated into one"
+                        className={css.aboutUsImg}
+                    />
+                    <Typography variant="h6" component="p" className={css.subtitle}>
+                        For more information, users can click to zoom into a county; they will see
+                        relative risk scores now calculated only relative to other counties in the
+                        same state and they can view a ‘report card’ for their county
+                    </Typography>
+                    <img
+                        src={landingpage3}
+                        alt="Different maps aggregated into one"
+                        className={css.aboutUsImg}
+                    />
+                    <img
+                        src={landingpage4}
+                        alt="Different maps aggregated into one"
+                        className={css.aboutUsImg}
+                    />
+                    <Typography variant="h6" component="p" className={css.subtitle}>
+                        In addition to allowing users to combine metrics, STRESS provides a
+                        centralized platform where users can easily view over 100 USA wide
+                        county-level datasets, over 20 country-level global datasets, and several
+                        city-level datasets from local partnerships. Although STRESS does not
+                        produce its own data, it provides an important service by bringing datasets
+                        from various agencies into one platform to increase accessibility. <br />{' '}
+                        <br />
+                        Data and maps for all metrics can be downloaded and used for further
+                        analysis or communication.
                     </Typography>
                 </Container>
             </main>
