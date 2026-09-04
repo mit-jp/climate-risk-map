ce9be711a9c17affa17076d6c77d6b352a632f6f
Thornblaster12345 <alexvinuk@icloud.com>
2026-09-04 12:53:13 -0400

    landing page navbar now highlights each tab

--- frontend/src/landing-page/LandingPageNavbar.tsx
+++ frontend/src/landing-page/LandingPageNavbar.tsx
@@ -1,30 +1,37 @@
-import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 import { Button } from '@mui/material'
+import { Link, NavLink } from 'react-router-dom'
 import css from '../Navigation.module.css'
+
+const navigationItems = [
+    { label: 'About', to: '/landing-page', end: true },
+    { label: 'Supporters and Collaborators', to: '/supporters-and-collaborators' },
+    { label: 'Tutorials and Use Cases', to: '/tutorials-and-use-cases' },
+    { label: 'Resources', to: '/resources' },
+    { label: 'FAQ', to: '/faq' },
+    { label: 'Methodology', to: '/methodology' },
+]
 
 function LandingPageNavbar() {
     return (
         <div className={css.navbar}>
             <nav className={css.nav}>
-                <Link to="/landing-page" className={css.a}>
-                    About
-                </Link>
-                <Link to="/supporters-and-collaborators" className={css.a}>
-                    Supporters and Collaborators
-                </Link>
-                <Link to="/tutorials-and-use-cases" className={css.a}>
-                    Tutorials and Use Cases
-                </Link>
-                <Link to="/resources" className={css.a}>
-                    Resources
-                </Link>
-                <Link to="/faq" className={css.a}>
-                    FAQ
-                </Link>
-                <Link to="/methodology" className={css.a}>
-                    Methodology
-                </Link>
+                {navigationItems.map((item) => (
+                    <NavLink
+                        key={item.to}
+                        to={item.to}
+                        end={item.end}
+                        className={({ isActive }) =>
+                            classNames(css.a, {
+                                [css.selected]: isActive,
+                            })
+                        }
+                    >
+                        {item.label}
+                    </NavLink>
+                ))}
             </nav>
+
             <Button component={Link} to="/" variant="contained" size="large">
                 Launch the STRESS Platform
             </Button>
