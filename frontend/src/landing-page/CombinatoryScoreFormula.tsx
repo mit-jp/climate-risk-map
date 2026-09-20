a766338db50321b16fa2956ce66a009ec58f282e
Thornblaster12345 <alexvinuk@icloud.com>
2026-09-19 20:36:46 -0400

    most of methodology is here, work in progress

---
+++ frontend/src/landing-page/CombinatoryScoreFormula.tsx
@@ -0,0 +1,56 @@
+import { Typography } from '@mui/material'
+
+function CombinatoryScoreFormula() {
+    return (
+        <Typography component="div">
+            <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+                <mrow>
+                    <msub>
+                        <mi>relative risk</mi>
+                        <mrow>
+                            <mi>m</mi>
+                            <mo>,</mo>
+                            <mi>c</mi>
+                        </mrow>
+                    </msub>
+                    <mo>=</mo>
+                    <msub>
+                        <mi>percentile</mi>
+                        <mrow>
+                            <mi>m</mi>
+                            <mo>,</mo>
+                            <mi>c</mi>
+                        </mrow>
+                    </msub>
+                    <mo>=</mo>
+                    <msub>
+                        <mi>p</mi>
+                        <mrow>
+                            <mi>m</mi>
+                            <mo>,</mo>
+                            <mi>c</mi>
+                        </mrow>
+                    </msub>
+                    <mo>=</mo>
+                    <mfrac>
+                        <mrow>
+                            <msub>
+                                <mi>rank</mi>
+                                <mrow>
+                                    <mi>m</mi>
+                                    <mo>,</mo>
+                                    <mi>c</mi>
+                                </mrow>
+                            </msub>
+                            <mo>×</mo>
+                            <mn>100</mn>
+                        </mrow>
+                        <mi>n</mi>
+                    </mfrac>
+                </mrow>
+            </math>
+        </Typography>
+    )
+}
+
+export default CombinatoryScoreFormula
--- frontend/src/landing-page/Methodology.tsx
+++ frontend/src/landing-page/Methodology.tsx
@@ -1,8 +1,11 @@
 import { Container, Typography } from '@mui/material'
 import css from './LandingPage.module.css'
+import methodologypage1 from './images/methodology_img1.png'
+import methodologypage2 from './images/methodology_img2.png'
 import Header from '../Header'
 import LandingPageNavbar from './LandingPageNavbar'
 import RiskFormula from './RiskFormula'
+import CombinatoryScoreFormula from './CombinatoryScoreFormula'
 
 function Methodology() {
     return (
@@ -38,7 +41,7 @@ function Methodology() {
                             </li>
                         </ol>
                     </Typography>
-                    <Typography variant="body1" className={css.subtitle}>
+                    <Typography variant="h6" component="p" className={css.subtitle}>
                         County-level USA-wide data that is uploaded can then be included in the
                         ‘combinatory metrics’ tab. The combinatory metrics tab shows ‘relative risk’
                         values, not the data in its native units. For each metric m, each county c
@@ -47,6 +50,34 @@ function Methodology() {
                         <RiskFormula />
                         where rank is defined as the number of counties with values less than or
                         equal to county c and n is the total number of counties.
+                    </Typography>
+                    <img
+                        src={methodologypage1}
+                        alt="A map showing the hottest monthly temperature in the US"
+                        className={css.aboutUsImg}
+                    />
+                    <Typography variant="subtitle1" className={css.caption}>
+                        Data from the ERA5 reanalysis showing the hottest average monthly
+                        temperature (in °C) from 2000-2019.
+                    </Typography>
+                    <img
+                        src={methodologypage2}
+                        alt="A map showing normalized hottest average monthly temperature in the US. Counties now have a relative risk score as opposed to showing the temperature in °C"
+                        className={css.aboutUsImg}
+                    />
+                    <Typography variant="subtitle1" className={css.caption}>
+                        Normalized values of the ERA5 reanalysis data of the hottest average monthly
+                        temperature from 2000-2019. Note that counties have the same ordinal ranking
+                        but now have a ‘relative risk score’ denoted by their percentile rankings,
+                        as opposed to simply showing the temperature in °C.
+                    </Typography>
+                    <Typography variant="h6" component="p" className={css.subtitle}>
+                        When multiple metrics are combined, a county’s relative risks (percentiles)
+                        are averaged to produce a combinatory risk score:
+                        <CombinatoryScoreFormula />
+                        Where the assigned weight, W_m, for each individual normalized risk metric,
+                        p_m,c, selected by the user is a constant value (from 0.1 to 1) across all
+                        counties.
                     </Typography>
                 </Container>
             </main>
